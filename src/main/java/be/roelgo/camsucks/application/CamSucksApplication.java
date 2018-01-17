f59905cbe8bd18b89a10687e64f725428464ea28
RoelGo <roelgoossens22@hotmail.com>
2018-01-17 22:11:45 +0100

    moved application to application package

--- src/main/java/be/roelgo/camsucks/application/CamSucksApplication.java
+++ src/main/java/be/roelgo/camsucks/application/CamSucksApplication.java
@@ -1,4 +1,4 @@
-package be.roelgo.camsucks;
+package be.roelgo.camsucks.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
