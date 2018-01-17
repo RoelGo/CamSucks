784314eebc75075402c7b4f22013a833bb2f0bad
RoelGo <roelgoossens22@hotmail.com>
2018-01-17 17:59:11 +0100

    Out with the old. In with the new.

---
+++ src/main/java/be/roelgo/camsucks/CamSucksApplication.java
@@ -0,0 +1,12 @@
+package be.roelgo.camsucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamSucksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamSucksApplication.class, args);
+	}
+}
