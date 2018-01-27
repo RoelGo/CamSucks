dc0a5fbedcebb854761a089f9bb9dbcb9a97373b
RoelGo <roelgoossens22@hotmail.com>
2018-01-27 15:50:16 +0100

    Removed 'header' from table and added Celsius sign

--- src/main/java/be/roelgo/camsucks/utils/TableUtils.java
+++ src/main/java/be/roelgo/camsucks/utils/TableUtils.java
@@ -1,7 +1,12 @@
 package be.roelgo.camsucks.utils;
 
 import be.roelgo.camsucks.service.model.SensorData;
-import org.springframework.shell.table.*;
+import org.springframework.shell.table.Table;
+import org.springframework.shell.table.TableBuilder;
+import org.springframework.shell.table.TableModel;
+import org.springframework.shell.table.TableModelBuilder;
+
+import static org.springframework.shell.table.BorderStyle.fancy_heavy;
 
 public class TableUtils {
 
@@ -12,22 +17,22 @@ public class TableUtils {
 
     public static Table buildTable(TableModel tableModel) {
         return new TableBuilder(tableModel)
-                .addFullBorder(BorderStyle.fancy_light_double_dash)
+                .addFullBorder(fancy_heavy)
                 .build();
     }
 
     public static TableModel buildTableModel(SensorData sensorData) {
         return new TableModelBuilder<String>()
                 .addRow()
-                .addValue("Name")
-                .addValue("Value")
-                .addRow()
                 .addValue("CPU")
-                .addValue(sensorData.getCpu().toString())
+                .addValue(temperature(sensorData.getCpu()))
                 .addRow()
                 .addValue("GPU")
-                .addValue(sensorData.getGpu().toString())
+                .addValue(temperature(sensorData.getGpu()))
                 .build();
     }
 
+    private static String temperature(Double temperature) {
+        return temperature + "°C" ;
+    }
 }
